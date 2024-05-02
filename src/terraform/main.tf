f7737eb5ae7ddc8fc3b3e89c45452a1dbec5585a
rabin <rabinmoharana143@gmail.com>
2024-05-03 01:02:23 +0530

    commit changes

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-2"
-  access_key = "AKIAZTSRGFKO5NTVAY32"
-  secret_key = "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"
+  access_key = "os.environ['access_key']"
+  secret_key = "os.environ['secret_key']"
 }
 
 resource "aws_s3_bucket" "my_bucket" {
