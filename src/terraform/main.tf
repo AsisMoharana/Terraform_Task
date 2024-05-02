0185fb837b6eff1d77f63767a2db12561a64adf7
rabin <rabinmoharana143@gmail.com>
2024-05-03 04:31:12 +0530

    create s3

--- src/terraform/main.tf
+++ src/terraform/main.tf
@@ -1,10 +1,15 @@
 provider "aws" {
-  region     = "us-east-2"
-  access_key = "os.environ['access_key']"
-  secret_key = "os.environ['secret_key']"
+  region     = "us-east-1"
+  access_key = "AKIAZTSRGFKO5NTVAY32"
+  secret_key = "Zdtc9bmBBwe92vg75E5eITtMcz6YSA+5jTsUeC8v"
 }
 
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "my-unique-bucket-name"  # Set your desired bucket name
-  acl    = "private"  # Set the ACL (Access Control List) for the bucket
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket1996rg"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
